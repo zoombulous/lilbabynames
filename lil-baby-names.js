96b9cf575a25412789cbdbb24a1bbdd87e7c365a
zoombulous <44047136+zoombulous@users.noreply.github.com>
2019-11-18 17:36:10 -0800

    Add files via upload

--- lil-baby-names.js
+++ lil-baby-names.js
@@ -6,6 +6,8 @@ new Vue({
         contains: '',
         startsWith: '',
         names: [],
+	sortKey: ['baby_name'],
+	sortOrder: ['asc'],
         last_event: 0,
         cancel: 0
     },
@@ -17,17 +19,34 @@ new Vue({
             this.last_event = t;
         }
     },
+    computed: {
+	namesSorted: function() {
+	    return _.orderBy(this.names, this.sortKey, this.sortOrder);
+	},
+    },
     methods: {
         findNames() {
             axios.get(
                 'http://names.sinistercode.com:4242/api/names?'
                     + 'format=json'
                     + '&sort=length-asc'
+		//this is called later, so I probably need to change here
                     + '&contains-letters=' + this.contains
                     + '&starts-with=' + this.startsWith
-            ).then((response) => {
-                    this.names = response.data.results.map(item => item.name);
-                });
-        }
+            ).then(function (response) => {
+                this.names = response.data.results.map(item => item.name);
+	    });.catch(function(error) {
+		console.log(error);
+            });
+	},
+	sortBy: function(key) {
+	    if (key == this.sortKey) {
+		this.sortOrder = (this.sortOrder
+				  == 'asc') ? 'desc' : 'asc';
+	    } else {
+		this.sortKey = key;
+		this.sortOrder = 'asc';
+	    }
+        },
     }
 });
